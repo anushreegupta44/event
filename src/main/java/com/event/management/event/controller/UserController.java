cc6cb155b9d22dea202edc7cfae51a7460ded81c
Anushree Gupta <ganu@thoughtworks.com>
2019-01-23 17:53:45 +0530

    [Anushree] Create user endpoint

--- src/main/java/com/event/management/event/controller/UserController.java
+++ src/main/java/com/event/management/event/controller/UserController.java
@@ -1,6 +1,24 @@
 package com.event.management.event.controller;
 
+import com.event.management.event.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class UserController {
 
+  @PostMapping("/user")
+  public ResponseEntity signUpUser(@RequestBody User user) {
+    Map<String, String> status = new HashMap() {{
+      put("status", "OK");
+    }};
+    return new ResponseEntity(status, HttpStatus.OK);
 
+  }
 }
